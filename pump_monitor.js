abdfe3c407c0d8dc9c4e2d788f883bc00b3b48ec
george redinger <george@georgeredinger.com>
2013-04-06 09:00:18 -0700

    alarm on off too long and on too long

--- pump_monitor.js
+++ pump_monitor.js
@@ -10,7 +10,14 @@ var Gpio = require('onoff').Gpio,
     power_start=Date.now(),
     power_stop =Date.now(); 
 
-console.log('Start');
+function ts() {
+  var now = new Date();
+  var jsonDate = now.toJSON();
+  return jsonDate;
+};
+
+
+console.log('Start ' +ts());
 
 pump.watch(function (err, value) {
   var state;
@@ -18,12 +25,11 @@ pump.watch(function (err, value) {
     state = pump.readSync(); 
     if(state == 1){
       power_start = Date.now();
-      console.log('off for '+(power_start-power_stop)/1000.0);
+      console.log(ts() +':off for '+((power_start-power_stop)/1000.0).toFixed());
     } else {
       power_stop  = Date.now();
-      console.log('on for '+(power_stop-power_start)/1000.0);
+      console.log(ts()+':on for '+((power_stop-power_start)/1000.0).toFixed());
     }
-    console.log('Pump ' + state);
      
 //    button.unexport(); // Unexport GPIO and free resources
 });
@@ -32,14 +38,14 @@ pump.watch(function (err, value) {
 setInterval(function() {
    now = Date.now();
    state = pump.readSync(); 
-
-   if((state == 1) && ((now - power_start) > 10.0*60.0*1000.0) { //pump on for > 10 minutes
-      console.log("timeout running for " + (now - power_start)/60000.0 + " minutes");
+   console.log("tick " + (process.uptime()/60.0).toFixed());
+   if((state == 1) && ((now - power_start) > 10.0*60.0*1000.0)) { //pump on for > 10 minutes
+      console.log(ts()+":alarm running for " + ((now - power_start)/60000.0).toFixed(2) + " minutes");
    };
 
    if((state == 0 ) && ((now - power_stop) > 60.0*60.0*1000.0)) { // pump off for > 1 hour
-      console.log("timeout off for " + (now - power_stop)/(60000.0) + " minutes");
+      console.log(ts()+":alarm off for " + ((now - power_stop)/(60000.0)).toFixed(2) + " minutes");
    }
 
-}, 10000);
+}, 60.0*1000.0*1.0); // once per minute
 
